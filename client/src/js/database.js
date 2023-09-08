717d866f48ec669289e62c39a5ed61ebf96c6594
Rupa Gadewar <rupa@gadewar.com>
2023-09-08 14:29:27 -0500

    Changed some logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,36 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+
+  const newItem = { content };
+  const id = await store.add(newItem);
+
+  await tx.done;
+  console.log(`Added item with ID: ${id}`);
+
+  db.close();
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+
+  const items = await store.getAll();
+
+  await tx.done;
+
+  db.close();
+  return items;
+};
+
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,63 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "index.html"),
+        filename: "index.html",
+        chunks: ["main"],
+      }),
+      // new HtmlWebpackPlugin({
+      //   template: path.resolve(__dirname, 'client/src/install.html'), // Specify your install HTML template
+      //   filename: 'install.html',
+      //   chunks: ['install'],
+      // }),
+      new WebpackPwaManifest({
+        name: "Your App Name",
+        short_name: "App",
+        description: "Description of your app",
+        background_color: "#ffffff",
+        theme_color: "#31a9e1",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"), // Path to your app's icon
+            sizes: [96, 128, 192, 256, 384, 512], // Specify icon sizes
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: path.resolve(__dirname, "src-sw.js"), // Path to your service worker file
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"], // Add CSS loaders
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader", // Add Babel loader
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
       ],
     },
   };
