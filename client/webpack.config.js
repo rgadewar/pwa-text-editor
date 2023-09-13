9f828f00f6d108f8d7284776b93f858b9916947f
Rupa Gadewar <rupa@gadewar.com>
2023-09-13 09:09:17 -0500

    Source worker logic change

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,60 +1,72 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WebpackPwaManifest = require("webpack-pwa-manifest");
-const path = require("path");
-const { InjectManifest } = require("workbox-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
 
 module.exports = () => {
   return {
-    mode: "development",
+    mode: 'development',
     entry: {
-      main: "./src/js/index.js",
-      install: "./src/js/install.js",
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Webpack plugin that generates our html file and injects our bundles
       new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "index.html"),
-        filename: "index.html",
-        chunks: ["main"],
+        template: './index.html',
+        title: 'JATE'
       }),
-      // new HtmlWebpackPlugin({
-      //   template: path.resolve(__dirname, 'client/src/install.html'), // Specify your install HTML template
-      //   filename: 'install.html',
-      //   chunks: ['install'],
-      // }),
+      // Injects our custom servie worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      // Creates a manifest.json file.
       new WebpackPwaManifest({
-        name: "Your App Name",
-        short_name: "App",
-        description: "Description of your app",
-        background_color: "#ffffff",
-        theme_color: "#31a9e1",
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Just another text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
-            src: path.resolve("src/images/logo.png"), // Path to your app's icon
-            sizes: [96, 128, 192, 256, 384, 512], // Specify icon sizes
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
           },
         ],
       }),
-      new InjectManifest({
-        swSrc: path.resolve(__dirname, "src-sw.js"), // Path to your service worker file
-      }),
+
     ],
+
     module: {
+      // CSS Loaders
       rules: [
         {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"], // Add CSS loaders
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
         },
         {
-          test: /\.js$/,
+          test: /\.m?js$/,
           exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
           use: {
-            loader: "babel-loader", // Add Babel loader
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"],
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
